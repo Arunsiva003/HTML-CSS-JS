a092d42c8c12ffe033edb93129ab519c2a63d3c6
Avisnura <99018821+Arunsiva003@users.noreply.github.com>
2023-04-03 18:52:41 +0530

    Add files via upload

---
+++ Task manager/toDoList.js
@@ -0,0 +1,71 @@
+window.addEventListener('load', () => {     
+    const form = document.querySelector('#new-task-form');
+    const input = document.querySelector("#new-task-input");
+    const lst = document.querySelector("#tasks");
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task=input.value;
+    if(!task){
+        alert("Input is Empty");
+        return;
+    }
+
+    const task_el = document.createElement("div");
+    task_el.classList.add("task");
+
+    const task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+
+    task_el.appendChild(task_content_el);
+
+    const task_inp_el = document.createElement("input");
+    task_inp_el.classList.add("text");
+    task_inp_el.type="text";
+    task_inp_el.value=task;
+    task_inp_el.setAttribute("readonly","readonly");
+
+    task_content_el.appendChild(task_inp_el);
+
+    const task_actions_el = document.createElement("div");
+    task_actions_el.classList.add("actions");
+
+    const task_edit_el = document.createElement("button");
+    task_edit_el.classList.add("edit");
+    task_edit_el.innerHTML="Edit";
+
+    const task_delete_el = document.createElement("button");
+    task_delete_el.classList.add("delete");
+    task_delete_el.innerHTML="Delete";
+
+    task_actions_el.appendChild(task_edit_el);
+    task_actions_el.appendChild(task_delete_el);
+
+    task_el.appendChild(task_actions_el); 
+
+    lst.appendChild(task_el);
+
+    input.value="";
+
+
+    task_edit_el.addEventListener("click", () => {
+        if(task_edit_el.innerText=="Edit"){
+            task_inp_el.removeAttribute("readonly");
+        task_inp_el.focus();
+        task_edit_el.innerText="Save";
+        }
+        else{
+            task_inp_el.setAttribute("readonly","readonly");
+            task_edit_el.innerHTML="Edit";
+        }
+    });
+
+    task_delete_el.addEventListener("click",() => {
+        lst.removeChild(task_el);
+    })
+
+
+
+    })
+})
